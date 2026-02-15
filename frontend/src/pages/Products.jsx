c2ac561b84cfa7ba19b932a62dec0677cb6d7627
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-15 09:40:05 +0530

    Git Commit Push

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -14,26 +14,27 @@ export default function Products() {
       <br />
       {products.map(p => (
         <div
-  key={p.id}
-  className="
+          key={p.id}
+          className="
    gap-4 h-110  p-3    group relative rounded-3xl bg-white/10 backdrop-blur-2xl border border-white/20 shadow-2xl overflow-hidden transition-all duration-500 hover:scale-[1.03]
 hover:shadow-[0_20px_60px_rgba(0,0,0,0.7)]
   "
->
-<img
-  src={`https://source.unsplash.com/400x300/?grocery,food&sig=${p.id}`}
+        >
+          <img 
+  src={`https://picsum.photos/400/300?randomfood=${p.id}`}
   alt="product"
-  className="w-full h-52 object-cover group-hover:scale-110 transition-transform duration-700"
+  className=" rounded-2xl w-full h-52 object-cover group-hover:scale-110 transition-transform duration-700"
 />
-<h2 className="text-xl font-bold mb-3 text-white">{p.name}</h2>
-            <span className="bg-green-500/20 text-green-400 px-3 py-1 rounded-full text-sm font-semibold">
-    ₹{p.price}
-  </span>
-<span className="bg-blue-500/20 text-blue-400 px-3 py-1 rounded-full text-sm font-semibold">
-    Stock: {p.stock}
-    
-  </span>
-    <button className="
+
+          <h2 className="text-xl font-bold mb-3 text-white">{p.name}</h2>
+          <span className="bg-green-500/20 text-green-400 px-3 py-1 rounded-full text-sm font-semibold">
+            ₹{p.price}
+          </span>
+          <span className="bg-blue-500/20 text-blue-400 px-3 py-1 rounded-full text-sm font-semibold">
+            Stock: {p.stock}
+
+          </span>
+          <button className="
       mt-6
       w-full
      bg-cyan-700
@@ -44,13 +45,13 @@ hover:shadow-[0_20px_60px_rgba(0,0,0,0.7)]
       font-semibold
       backdrop-blur-lg
       text-white
-    ">
-    Add to Cart
-  </button>
+    "> 
+            Add to Cart
+          </button>
 
-  
+
         </div>
-        
+
       ))}
     </div>
   )
